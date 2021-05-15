0eb88e62a51971ab5461b3533677ec0cb930b3ef
cloxnu <o18089541818@outlook.com>
2021-05-15 16:03:43 +0800

    Update chart

--- app/src/main/java/com/example/android_get_message/MainActivity.java
+++ app/src/main/java/com/example/android_get_message/MainActivity.java
@@ -10,22 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     IntentFilter filter;
-//    GetMessage receiver;
-//    private SmsObserver smsObserver;
 
-
-//    @SuppressLint("HandlerLeak")
-//    private final Handler smsHandler = new Handler() {
-//        @Override
-//        public void handleMessage(Message msg) {
-//            System.out.println("Get3");
-//            System.out.println(msg.toString());
-//        }
-//
-//    };
-
+    private LineChart lineChart;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -36,18 +31,20 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, 1000);
         }
-//
-//        if(ContextCompat.checkSelfPermission(this, "android.permission_RECEIVE_SMS") == PackageManager.PERMISSION_GRANTED){
-//            System.out.println("permission got");
-//            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
-//
-//            } else  {
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 0);
-//            }
-//        }
-//        Toast.makeText(this, "new", Toast.LENGTH_LONG).show();
 
+        lineChart = findViewById(R.id.chart);
+        LineData data = new LineData(getData());
+        lineChart.setData(data);
+        lineChart.invalidate();
     }
+
+    private LineDataSet getData() {
+        LineDataSet dataSet = new LineDataSet(new ArrayList<>(), "Air Pollution Dataset");
+        dataSet.addEntry(new Entry(1, 1000));
+        return dataSet;
+    }
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int [] grantResults){
         if (requestCode == 1000){
@@ -67,63 +64,4 @@ public class MainActivity extends AppCompatActivity {
 //        unregisterReceiver(receiver);//解绑广播接收器
     }
 
-
-//    public static boolean hasPermissions(Context context, String... permissions) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
-//            for (String permission : permissions) {
-//                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//
-//    public void askPermissions() {
-//
-//        String[] PERMISSIONS = {Manifest.permission.READ_SMS};
-//
-//        if (!hasPermissions(this, PERMISSIONS)) {
-//            ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
-//        }
-//        System.out.println("----------------------------" + hasPermissions(this,PERMISSIONS));
-//
-//    }
-
 }
-
-//class SmsObserver extends ContentObserver {
-//    private Uri uri;
-//    private Context mContext;
-//    private Handler mHandler;
-//    private String[] projection = new String[]{"_id","body"};
-//    private Cursor cursor = null;
-//    public SmsObserver(Handler handler) {
-//        super(handler);
-//    }
-//
-//    public SmsObserver(Context context,Handler handler){
-//        super(handler);
-//        this.mContext = context;
-//        this.mHandler = handler;
-//    }
-//
-//    @Override
-//    public void onChange(boolean selfChange, Uri uri) {
-//        super.onChange(selfChange);
-//        if (uri.toString().equals("content://sms/raw")) {
-//
-//            System.out.println("Get1");
-//            return;
-//        }
-//        System.out.println("Get");
-//        cursor = mContext.getContentResolver().query(Uri.parse("content://sms/inbox"), projection,null, null, "_id desc");
-//        if (cursor != null && cursor.getCount() != 0) {
-//            cursor.moveToNext();
-//            int smsbodyColumn = cursor.getColumnIndex("body");
-//            String smsBody = cursor.getString(smsbodyColumn);
-//            System.out.println("Haha: " + smsBody);
-//        }
-//        cursor.close();
-//    }
-//}
